2318b70a532759e102a00dae50d08593882b719e
Tiffany Stallings <je.deviens.moi@gmail.com>
2016-03-05 19:55:21 -0500

    Add text to start button
    Added a border to the start button, as well as some text.
    
    TODO: Add a hover effect

--- js/levels.js
+++ js/levels.js
@@ -1,10 +1,9 @@
 var splashScreen = function() {
-	ctx.fillStyle = "#000";
-	ctx.fillRect(0,0, canvas.width, canvas.height)
+
 
 	//Define and draw the start button.
 	button = new Button(canvas.width/2-150, canvas.height/2-50,
-		300, 100)
+		300, 100, 'Play!')
 	button.draw();
 
 	//Check if start button has been clicked.
@@ -19,28 +18,36 @@ var splashScreen = function() {
 		if (checkCollision(button, clickObj)) {
 			button.clicked();
 		}
-
-		else {
-			button.draw();
-		}
 	});
 }
 
-var Button = function(x, y, w, h) {
+var Button = function(x, y, w, h, text) {
 	this.x = x;
 	this.y = y;
 	this.w = w;
 	this.h = h;
+	this.text = text;
 }
 
+// Initialized main on button click
 Button.prototype.clicked = function() {;
 	ctx.fillstyle = '#fff';
-	ctx.clearRect(0,0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	main();
 }
 
 Button.prototype.draw = function() {
 	ctx.fillStyle = '#4e66d2';
+	ctx.strokeStyle = '#fff'
+	ctx.lineWidth = 5;
 	ctx.fillRect(button.x, button.y, button.w, button.h);
+	ctx.strokeRect(button.x, button.y, button.w, button.h);
+
+	ctx.font = '36pt Impact';
+	ctx.textAlign = 'center';
+	ctx.textBaseline = 'middle';
+	ctx.fillStyle = '#fff';
+	ctx.fillText(button.text, button.x + button.w/2,
+		button.y + button.h/2);
 }
 
